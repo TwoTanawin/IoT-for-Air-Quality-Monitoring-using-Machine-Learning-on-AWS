c8c16410b27ece20614421a2849086efd60d5f97
twotanawin <tanawin.twox@gmail.com>
2024-03-17 15:00:22 +0700

    method

--- web-app/basic-Vue/my-app/src/App.vue
+++ web-app/basic-Vue/my-app/src/App.vue
@@ -2,8 +2,8 @@
   <!-- <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <section>
-    <h1>Register : {{firstName}}</h1>
-    <h1>Register fullname : {{ lastName }}</h1>
+    <!-- <h1>Register : {{firstName}}</h1> -->
+    <h1>Register fullname : {{ getFullName() }}</h1>
     <h1>Age : {{ age }}</h1>
     <p>Address : <span v-html="address"></span></p>
   </section>
@@ -23,6 +23,12 @@ export default {
       lastName:"Tanawin",
       age:23,
       address:"<i>Bangkok</i>"
+    }
+  },
+  methods:{
+    getFullName(){
+      // return this.firstName + this.lastName
+      return `${this.firstName+" "+this.lastName}`
     }
   }
 }
